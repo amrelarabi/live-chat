3f6ee466a01b4d3ac52243a8c34e7970c056b79b
amrelarabi <amr.elarabi.egy@gmail.com>
2020-11-02 22:58:23 +0200

    Fix room url

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('room/<str:room_name>/', views.room, name='room'),
 ]
